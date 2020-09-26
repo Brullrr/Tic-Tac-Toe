eedd2cb2501292484ab1c359a09b3a1a28865ccf
Brullrr <rbrull@yahoo.com>
2020-09-27 01:20:41 +0900

    Updates

---
+++ precache-manifest.f58e8bc3fdfce234e00f8c4efd526dcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c12589a79669ea2af71e3b77e285569",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "a351af5612025d53a6a5",
+    "url": "/Tic-Tac-Toe/static/css/main.e9e9ded4.chunk.css"
+  },
+  {
+    "revision": "539986e05730c993f493",
+    "url": "/Tic-Tac-Toe/static/js/2.560323bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.560323bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a351af5612025d53a6a5",
+    "url": "/Tic-Tac-Toe/static/js/main.3fb98ac6.chunk.js"
+  },
+  {
+    "revision": "5965f36d7f72120b2bad",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.49965c0b.js"
+  },
+  {
+    "revision": "78e6d8c3e6509be713b37bbd8b078bdb",
+    "url": "/Tic-Tac-Toe/static/media/capital_o.78e6d8c3.png"
+  },
+  {
+    "revision": "c53f595005a514058b112df420d2077b",
+    "url": "/Tic-Tac-Toe/static/media/capital_x.c53f5950.png"
+  }
+]);
